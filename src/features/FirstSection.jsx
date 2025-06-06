1766666d711ea71681b39aae0abb4b76455bd22d
Hyun <dacapo629@naver.com>
2025-06-07 00:36:16 +0900

    홈페이지 메인 섹션 첫번째 코딩 시작

---
+++ src/features/FirstSection.jsx
@@ -0,0 +1,9 @@
+import '../styles/firstSection.css';
+const FirstSection = () => {
+    return(
+        <div className="section-container">
+            첫번쨰 섹션입니다.
+        </div>
+    )
+}
+export default FirstSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import FirstSection from "../features/FirstSection";
 import Header from "../features/Header";
 
 const Home = () => {
     return(
         <div>
             <Header/>
+            <FirstSection/>
         </div>
     )
 }
